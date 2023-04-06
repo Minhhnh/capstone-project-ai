2419f435eee643e5b1c4506c221fd605908135a1
Hồ Ngọc Hoàng Minh <minhhnh@bap.jp>
2023-04-06 14:39:27 +0700

    Remove unuse files

--- app/api/database/models/response.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class InterrogateResponse(BaseModel):
-    caption: str = Field(default=None, title="Caption",
-                         description="The generated caption for the image.")
